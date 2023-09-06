4654b1f05d1fdde1d471b996daa2b9d4e882969a
hitesh34 <79905249+hitesh34@users.noreply.github.com>
2023-09-06 20:22:04 +0530

    Update BlogComponent.js

--- src/Components/BlogComponent.js
+++ src/Components/BlogComponent.js
@@ -132,15 +132,17 @@ function BlogComponent() {
 
 
   return (
-      <div className="bg-white py-24 sm:py-32">
-        <div className="mx-auto max-w-7xl px-6 lg:px-8">
-          <div className="mx-auto max-w-2xl text-center">
-            <h2 className="text-3xl font-bold text-gray-900 sm:text-4xl">From your blog</h2>
-            <p className="mt-2 text-lg text-gray-600">Learn how to grow your business with our expert advice.</p>
-          </div>
-          <div className="mx-auto mt-16 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-20 lg:mx-0 lg:max-w-none lg:grid-cols-3">
-            {posts.map((post) => (
-              <article key={post.id} className="bg-white rounded-lg shadow-md overflow-hidden">
+    <div className="bg-white py-24 sm:py-32">
+      <div className="mx-auto max-w-7xl px-6 lg:px-8">
+        <div className="mx-auto max-w-2xl text-center">
+          <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">From the blog</h2>
+          <p className="mt-2 text-lg leading-8 text-gray-600">
+            Learn how to grow your business with our expert advice.
+          </p>
+        </div>
+        <div className="mx-auto mt-16 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-20 lg:mx-0 lg:max-w-none lg:grid-cols-3">
+          {posts.map((post) => (
+            <article key={post.id} className="bg-white rounded-lg shadow-md overflow-hidden">
               <div className="relative w-full">
                 <img
                   src={post.image_url || 'https://picsum.photos/200'}
@@ -152,14 +154,16 @@ function BlogComponent() {
               <div className="max-w-xl">
                 <div className="mt-8 flex items-center gap-x-4 text-xs">
                   <time dateTime={post.pub_date} className="text-gray-500">
-                    {post.pub_date}
+                    {post.publication_date} {/* Display the publication date */}
                   </time>
-                  <a
-                    href="#"
-                    className="relative z-10 rounded-full bg-gray-50 px-3 py-1.5 font-medium text-gray-600 hover:bg-gray-100"
-                  >
-                    {post.category}
-                  </a>
+                  {post.category && post.category.href && (
+                    <a
+                      href={post.category.href}
+                      className="relative z-10 rounded-full bg-gray-50 px-3 py-1.5 font-medium text-gray-600 hover:bg-gray-100"
+                    >
+                      {post.category} {/* Display the category */}
+                    </a>
+                  )}
                 </div>
                 <div className="group relative center">
                   <h3 className="mt-3 text-lg font-semibold leading-6 text-gray-900 group-hover:text-gray-600 center">
@@ -168,7 +172,10 @@ function BlogComponent() {
                       {post.title}
                     </Link>
                   </h3>
-                  <p className="mt-5 line-clamp-3 text-sm leading-6 text-gray-600 center">{post.content}</p>
+                  {/* Add initial lines of blog post content here */}
+                  <p className="mt-2 text-sm leading-6 text-gray-600 center">
+                    {post.subtitle} {/* Display the subtitle */}
+                  </p>
                 </div>
                 <div className="relative mt-8 flex items-center gap-x-4">
                   {post.author && post.author.imageUrl ? (
@@ -185,12 +192,7 @@ function BlogComponent() {
                         </a>
                       </p>
                     ) : (
-                      <p className="text-gray-900">Unknown Author</p>
-                    )}
-                    {post.author ? (
-                      <p className="text-gray-600">{post.author.role}</p>
-                    ) : (
-                      <p className="text-gray-600">Author Role</p>
+                      <p className="text-gray-900">HomeOpen</p>
                     )}
                   </div>
                 </div>
@@ -208,35 +210,35 @@ function BlogComponent() {
         <div className="flex justify-center mt-8 space-x-4">
           {/* Pagination Buttons */}
           {currentPage > 1 && (
-    <button
-      className="px-4 py-2 rounded bg-gray-300 text-gray-700 hover:bg-gray-400 transition duration-300"
-      onClick={() => handlePageChange(currentPage - 1)}
-    >
-      Previous
-    </button>
-  )}
-  {Array.from({ length: Math.ceil(totalPages / 10) }).map((_, index) => (
-    <button
-      key={index + 1}
-      className={`px-4 py-2 rounded ${
-        currentPage === index + 1
-          ? 'bg-blue-500 text-white'
-          : 'bg-gray-300 text-gray-700 hover:bg-gray-400 transition duration-300'
-      }`}
-      onClick={() => handlePageChange(index + 1)}
-    >
-      {index + 1}
-    </button>
-  ))}
-  {currentPage < Math.ceil(totalPages / 10) && (
-    <button
-      className="px-4 py-2 rounded bg-gray-300 text-gray-700 hover:bg-gray-400 transition duration-300"
-      onClick={() => handlePageChange(currentPage + 1)}
-    >
-      Next
-    </button>
-  )}
-    </div>
+            <button
+              className="px-4 py-2 rounded bg-gray-300 text-gray-700 hover:bg-gray-400 transition duration-300"
+              onClick={() => handlePageChange(currentPage - 1)}
+            >
+              Previous
+            </button>
+          )}
+          {Array.from({ length: Math.ceil(totalPages / 10) }).map((_, index) => (
+            <button
+              key={index + 1}
+              className={`px-4 py-2 rounded ${
+                currentPage === index + 1
+                  ? 'bg-blue-500 text-white'
+                  : 'bg-gray-300 text-gray-700 hover:bg-gray-400 transition duration-300'
+              }`}
+              onClick={() => handlePageChange(index + 1)}
+            >
+              {index + 1}
+            </button>
+          ))}
+          {currentPage < Math.ceil(totalPages / 10) && (
+            <button
+              className="px-4 py-2 rounded bg-gray-300 text-gray-700 hover:bg-gray-400 transition duration-300"
+              onClick={() => handlePageChange(currentPage + 1)}
+            >
+              Next
+            </button>
+          )}
+        </div>
       </div>
     </div>
   );
